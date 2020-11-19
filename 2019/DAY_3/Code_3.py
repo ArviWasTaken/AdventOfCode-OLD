3ad834f4dae3489726c1f942f25ab1c11c67fee5
ArviWasTaken <arvid.smit@xs4all.nl>
2020-11-19 23:13:20 +0100

    inital commit v.2

--- 2019/DAY_3/Code_3.py
+++ 2019/DAY_3/Code_3.py
@@ -14,45 +14,48 @@ for command_nm in range(len(a_wire)):
     if command[0] == "U":
         for i in range(int(command[1:])):
             x += 1
-            coor = f"{x},{y}"
+            coor = [x, y]
             a_wire_list.append(coor)
     elif command[0] == "D":
         for i in range(int(command[1:])):
             x -= 1
-            coor = f"{x},{y}"
+            coor = [x, y]
             a_wire_list.append(coor)
     elif command[0] == "L":
         for i in range(int(command[1:])):
             y -= 1
-            coor = f"{x},{y}"
+            coor = [x, y]
             a_wire_list.append(coor)
     elif command[0] == "R":
         for i in range(int(command[1:])):
             y += 1
-            coor = f"{x},{y}"
+            coor = [x, y]
             a_wire_list.append(coor)
+
+x = 0
+y = 0
 
 for command_nm in range(len(b_wire)):
     command = b_wire[command_nm]
     if command[0] == "U":
         for i in range(int(command[1:])):
             x += 1
-            coor = f"{x},{y}"
+            coor = [x, y]
             b_wire_list.append(coor)
     elif command[0] == "D":
         for i in range(int(command[1:])):
             x -= 1
-            coor = f"{x},{y}"
+            coor = [x, y]
             b_wire_list.append(coor)
     elif command[0] == "L":
         for i in range(int(command[1:])):
             y -= 1
-            coor = f"{x},{y}"
+            coor = [x, y]
             a_wire_list.append(coor)
     elif command[0] == "R":
         for i in range(int(command[1:])):
             y += 1
-            coor = f"{x},{y}"
+            coor = [x, y]
             b_wire_list.append(coor)
 
 
@@ -65,10 +68,16 @@ else:
         if b_wire_list[i] in a_wire_list:
             cross_list.append(b_wire_list[i])
 print(len(cross_list))
-print(cross_list)
+print(sorted(cross_list))
 smallest = [10000, 1000]
+
 for i in range(len(cross_list)):
-    if int(cross_list[i][0]) < int(smallest[0]) and int(cross_list[i][1]) < int(smallest[1]):
+    if cross_list[i][0] < 0:
+        cross_list[i][0] = abs(cross_list[i][0])
+    if cross_list[i][1] < 0:
+        cross_list[i][1] = abs(cross_list[i][1])
+    if sum(cross_list[i]) < sum(smallest):
         smallest[0] = cross_list[i][0]
         smallest[1] = cross_list[i][1]
+print(cross_list)
 print(smallest)
