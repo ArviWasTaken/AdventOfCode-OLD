5757e5cc3f3b92cfaae286ee5afd74306446b373
ArviWasTaken <arvid.smit@xs4all.nl>
2020-11-30 01:10:10 +0100

    Rework 5 fromm the bottom: ADDED opcode 3&4

--- 2019/DAY_5/Code_5.py
+++ 2019/DAY_5/Code_5.py
@@ -1,5 +1,6 @@
-file = open("PuzzleInput_5.txt", "r")
+file = open("PuzzleInput_2.txt", "r")
 line = file.readline()
+line = "3,0,4,0,99"
 intcode = line.split(",")
 
 
@@ -10,27 +11,47 @@ for x in range(len(intcode)):
     memory.append(int(intcode[x]))
 
 
+
 while True:
     opcode = memory[instruction_pointer]
-
-
-    #-------------------OPERATIONS---------------------#
 
     # END
     if opcode == 99:
         break
-    parameters_1 = memory[instruction_pointer + 1]
-    parameters_2 = memory[instruction_pointer + 2]
-    adrress = memory[instruction_pointer + 3]
+
+    #-------------------OPERATIONS---------------------#
+
 
     # ADD instruction
     if opcode == 1:
+
+        parameters_1 = memory[instruction_pointer + 1]
+        parameters_2 = memory[instruction_pointer + 2]
+        adrress = memory[instruction_pointer + 3]
+
         memory[adrress] = memory[parameters_1] + memory[parameters_2]
         instruction_pointer += 4
 
     # MULTIPLY instruction
     elif opcode == 2:
+        parameters_1 = memory[instruction_pointer + 1]
+        parameters_2 = memory[instruction_pointer + 2]
+        adrress = memory[instruction_pointer + 3]
+
         memory[adrress] = memory[parameters_1] * memory[parameters_2]
         instruction_pointer += 4
 
-print(memory[0])
+    #INPUT instruction
+    elif opcode == 3:
+        parameters_1 = memory[instruction_pointer + 1]
+
+        memory[parameters_1] = input("Geef input:")
+        instruction_pointer += 2
+
+    #OUTPUT instruction
+    elif opcode == 4:
+        parameters_1 = memory[instruction_pointer + 1]
+
+        print(memory[parameters_1])
+        instruction_pointer += 2
+
