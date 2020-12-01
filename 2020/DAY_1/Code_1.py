85829e35f13db5a1ce38b62ed444605c15567f1d
ArviWasTaken <arvid.smit@xs4all.nl>
2020-12-01 06:42:01 +0100

    Day 1 2020 b

--- 2020/DAY_1/Code_1.py
+++ 2020/DAY_1/Code_1.py
@@ -1,11 +1,28 @@
-file = open("PuzzleInput_1.txt", "r")
-expenses = []
-for x in file.readlines():
-    x = x[:-1]
-    x = int(x)
-    expenses.append(x)
-print(expenses)
-for e in expenses:
-    for y in expenses:
-        if e + y == 2020:
-            print(e * y)
+def part_1():
+    file = open("PuzzleInput_1.txt", "r")
+    expenses = []
+    for x in file.readlines():
+        x = x[:-1]
+        x = int(x)
+        expenses.append(x)
+    for e in expenses:
+        for y in expenses:
+            if e + y == 2020:
+                print(e * y)
+
+
+
+def part_2():
+    file = open("PuzzleInput_1.txt", "r")
+    expenses = []
+    for x in file.readlines():
+        x = x[:-1]
+        x = int(x)
+        expenses.append(x)
+    for e in expenses:
+        for y in expenses:
+            for x in expenses:
+                if e + y + x == 2020:
+                    print(e * y * x)
+part_1()
+part_2()
