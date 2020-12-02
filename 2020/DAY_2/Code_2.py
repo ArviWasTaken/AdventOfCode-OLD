a45db6c9f99bfa5375fef3452264a042b8007ed9
ArviWasTaken <arvid.smit@xs4all.nl>
2020-12-02 07:28:09 +0100

    Day 2 2020 Finished

--- 2020/DAY_2/Code_2.py
+++ 2020/DAY_2/Code_2.py
@@ -6,7 +6,7 @@ valid_pass = 0
 for x in file.readlines():
     x = x.split(":")
     y = x[1]
-    y =y.strip()
+    y = y.strip()
     x = x[0].split(" ")
     x[0] = x[0].split("-")
     passwords[keys] = [x, y]
@@ -17,9 +17,6 @@ for x in passwords.values():
     letter = x[0][1]
     min = int(x[0][0][0])
     max = int(x[0][0][1])
-    if x[1][min - 1] == letter or x[1][max - 1] == letter:
-        if x[1][min - 1] != x[1][max - 1]:
-            valid_pass += 1
+    if (x[1][min - 1] == letter or x[1][max - 1] == letter) and x[1][min - 1] != x[1][max - 1]:
+        valid_pass += 1
 print(valid_pass)
-
-
