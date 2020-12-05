c8bbedbe826ef9a8e7699ddcac83cea80d7e77bc
ArviWasTaken <arvid.smit@xs4all.nl>
2020-12-05 07:33:54 +0100

    2020 day 5 finished

--- 2020/DAY_05/Code_5.py
+++ 2020/DAY_05/Code_5.py
@@ -19,13 +19,13 @@ for x in input:
         diffrence_col = upper_column - lower_column
         difference_seat = upper_seat - lower_seat
 
-        if diffrence_col == 2 and column_set == False:
+        if diffrence_col == 1 and column_set == False:
             if y == "F":
                 column = lower_column
             elif y == "B":
                 column = upper_column
             column_set = True
-        elif difference_seat == 2 and seat_set == False:
+        elif difference_seat == 1 and seat_set == False:
             if y == "L":
                 seat = lower_seat
 
@@ -34,20 +34,22 @@ for x in input:
             seat_set = True
 
         elif y == "F":
-            upper_column -= math.floor(diffrence_col / 2.0)
+            upper_column -= math.ceil(diffrence_col / 2.0)
 
         elif y == "B":
             lower_column += math.ceil(diffrence_col / 2.0)
 
         elif y == "L":
-            upper_seat -= math.floor(difference_seat / 2.0)
+            upper_seat -= math.ceil(difference_seat / 2.0)
 
         elif y == "R":
             lower_seat += math.ceil(difference_seat / 2.0)
 
     seat_id.append(8 * column + seat)
 
+
 for x in seat_id:
     if x+1 not in seat_id and x+2 in seat_id:
-        print(x)
+        print(x+1)
+
 
