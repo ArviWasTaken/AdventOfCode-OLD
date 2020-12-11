592e609df9f893e76b41b75a1135c1ea27245c93
ArviWasTaken <arvid.smit@xs4all.nl>
2020-12-11 09:36:36 +0100

    2020: Day 11 finalized

--- 2020/DAY_11/Code_11_2.py
+++ 2020/DAY_11/Code_11_2.py
@@ -31,6 +31,76 @@ while changed:
         if dict[coor] == ".":
             continue
 
+        for tmp_x in range(x-1, -1, -1): #Naar links
+            if dict[tmp_x, y] == "L" or dict[tmp_x, y] == "#":
+                neighbours.append([tmp_x, y])
+                break
+        for tmp_x in range(x+1, max_x+1, 1): #Naar rechts
+            if dict[tmp_x, y] == "L" or dict[tmp_x, y] == "#":
+                neighbours.append([tmp_x, y])
+                break
+
+        for tmp_y in range(y-1, -1, -1): #Naar boven
+            if dict[x, tmp_y] == "L" or dict[x, tmp_y] == "#":
+                neighbours.append([x, tmp_y])
+                break
+        for tmp_y in range(y+1, max_y+1, 1): #Naar onder
+            if dict[x, tmp_y] == "L" or dict[x, tmp_y] == "#":
+                neighbours.append([x, tmp_y])
+                break
+
+        if x < y:
+            rng = x
+        else:
+            rng = y
+        for tmp_offset in range(1, rng+1, 1): #Naar linksboven
+            tmp_x = x - tmp_offset
+            tmp_y = y - tmp_offset
+            if x == tmp_x:
+                continue
+            if dict[tmp_x, tmp_y] == "L" or dict[tmp_x, tmp_y] == "#":
+                neighbours.append([tmp_x, tmp_y])
+
+                break
+        if max_x - x < max_y - y:
+            rng = max_x - x
+        else:
+            rng = max_y - y
+        for tmp_offset in range(1, rng+1, 1): #Naar rechtsbeneden
+            tmp_x = x + tmp_offset
+            tmp_y = y + tmp_offset
+            if x == tmp_x:
+                continue
+            if dict[tmp_x, tmp_y] == "L" or dict[tmp_x, tmp_y] == "#":
+                neighbours.append([tmp_x, tmp_y])
+                break
+
+        if x < max_y - y:
+            rng = x
+        else:
+            rng = max_y - y
+        for tmp_offset in range(1, rng + 1, 1):  # Naar linksbeneden
+            tmp_x = x - tmp_offset
+            tmp_y = y + tmp_offset
+            if x == tmp_x:
+                continue
+            if dict[tmp_x, tmp_y] == "L" or dict[tmp_x, tmp_y] == "#":
+                neighbours.append([tmp_x, tmp_y])
+                break
+
+        if max_x - x < y:
+            rng = max_x - x
+        else:
+            rng = y
+        for tmp_offset in range(1, rng + 1, 1):  # Naar rechtsboven
+            tmp_x = x + tmp_offset
+            tmp_y = y - tmp_offset
+            if x == tmp_x :
+                continue
+            if dict[tmp_x, tmp_y] == "L" or dict[tmp_x, tmp_y] == "#":
+                neighbours.append([tmp_x, tmp_y])
+                break
+
 
         seat_occupied = 0
         for neigh_coor in neighbours:
@@ -45,10 +115,11 @@ while changed:
         if dict[x, y] == "L" and seat_occupied == 0:
             tmp_dict[x, y] = "#"
             changed = True
-        elif dict[x, y] == "#" and seat_occupied >= 4:
+        elif dict[x, y] == "#" and seat_occupied >= 5:
             tmp_dict[x, y] = "L"
             changed = True
     dict = tmp_dict
+
 
 hastag_counter = 0
 for x in dict.values():
