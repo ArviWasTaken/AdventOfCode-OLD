398cb0987644d817fa4f61579d555e0b7a4f6bbe
Arvid Smit <arvid.smit@xs4all.nl>
2020-12-21 07:27:18 +0100

    2020 day 21: 27 lines in 90 mins, fuck yhea

--- 2020/DAY_21/Code_21.py
+++ 2020/DAY_21/Code_21.py
@@ -1,7 +1,27 @@
 file = open("PuzzleInput_21.txt", "r")
 
-dict
+dict = {}
+count = {}
 for line in file.readlines():
     line = line.strip()
+    line = line.split(" (")
+    allergen = line[-1]
+    allergen = allergen[:-1]
+    allergen = allergen.split(", ")
+    line.pop(-1)
+    line = str(line[0])
     line = line.split()
-    print(line)
+    for alergeen in allergen:
+        if alergeen in dict.keys():
+            already_al = dict[alergeen]
+            for ingre in line:
+                already_al.append(ingre)
+            dict[alergeen] = already_al
+            count[alergeen] = count[alergeen] + 1
+        else:
+            dict[alergeen] = line
+            count[alergeen] = 1
+print(dict)
+print(count)
+
+for aler, ingredienten in dict.items():
