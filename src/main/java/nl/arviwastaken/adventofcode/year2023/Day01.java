ae977d6ba9ab7dabdd1ad4fe085020a5fdf2c3e1
Arvid Smit <arvid.code@freedom.nl>
2023-12-01 07:39:44 +0100

    2023 day 01: simplified word to digit conversion

--- src/main/java/nl/arviwastaken/adventofcode/year2023/Day01.java
+++ src/main/java/nl/arviwastaken/adventofcode/year2023/Day01.java
@@ -35,7 +35,6 @@ public class Day01 extends Solution<List<String>> {
                 String firstLast = String.valueOf(numbers.charAt(0)) + numbers.charAt(numbers.length() -1);
                 total += Integer.parseInt(firstLast);
             }
-
         }
         return String.valueOf(total);
     }
@@ -44,8 +43,8 @@ public class Day01 extends Solution<List<String>> {
     public String part2(List<String> input) {
         // Put your solution for part 2 here
         List<String> words = new ArrayList<>(Arrays.asList("one", "two", "three", "four", "five", "six", "seven", "eight", "nine"));
-
         int total = 0;
+
         for (String s: input
         ) {
             String numbers = "";
@@ -57,7 +56,7 @@ public class Day01 extends Solution<List<String>> {
                     for (int j= 0; j < words.size(); j++) {
                         String sub = s.substring(i);
                         if (sub.indexOf(words.get(j)) == 0) {
-                            numbers += wordToDigit(sub.substring(0, words.get(j).length()));
+                            numbers += String.valueOf(j + 1);
                         }
                     }
                 }
@@ -74,30 +73,5 @@ public class Day01 extends Solution<List<String>> {
         List<String> output = InputUtil.getInput("2023", "1", true);
         // Put the code to convert from a list with lines to usuable objects here
         return output;
-    }
-
-    private String wordToDigit(String s) {
-        switch (s) {
-            case "one":
-                return "1";
-            case "two":
-                return "2";
-            case "three":
-                return "3";
-            case "four":
-                return "4";
-            case "five":
-                return "5";
-            case "six":
-                return "6";
-            case "seven":
-                return "7";
-            case "eight":
-                return "8";
-            case "nine":
-                return "9";
-            default:
-                return "default";
-        }
     }
 }
